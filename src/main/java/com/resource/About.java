0a85cc48f51d683f9af6efd6efa8c22a74573b0c
Ankur Srivastava <Ankur.Srivastava@nike.com>
2016-03-02 23:34:50 -0800

    adding some more code to start working

--- src/main/java/com/resource/About.java
+++ src/main/java/com/resource/About.java
@@ -1,8 +1,10 @@
 package com.resource;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 import java.util.HashMap;
@@ -11,7 +13,8 @@ import java.util.HashMap;
 /**
  * Created by ankcrimson on 3/2/16.
  */
-@Path("about")
+@Path("/about")
+@ApplicationPath("test")
 public class About {
 
     @GET
@@ -20,7 +23,8 @@ public class About {
         Map<String,Object> aboutResponse = new HashMap<>();
         aboutResponse.put("name","MicroService");
         aboutResponse.put("version","v1");
-        return Response.ok(aboutResponse).build();
+        GenericEntity<Map<String , Object>> entity = new GenericEntity<Map<String,Object>>(aboutResponse){};
+        return Response.ok(entity).build();
     }
 
 }
