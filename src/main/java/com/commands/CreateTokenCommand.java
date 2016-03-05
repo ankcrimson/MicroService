3566fab8347b3b430762433f11fcf29bafd4d6f1
Ankur Srivastava <Ankur.Srivastava@nike.com>
2016-03-05 15:32:25 -0800

    updated for timeout

--- src/main/java/com/commands/CreateTokenCommand.java
+++ src/main/java/com/commands/CreateTokenCommand.java
@@ -6,7 +6,10 @@ import com.dto.TokenResponseDTO;
 import com.models.CouchbaseDatasource;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
 import com.services.DatabaseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 
@@ -16,28 +19,28 @@ import javax.inject.Inject;
 public class CreateTokenCommand extends HystrixCommand<String> {
 
     private DatabaseService service;
-
     private TokenRequestDTO tokenRequestDTO;
 
+    private static Logger LOGGER = LoggerFactory.getLogger(CreateTokenCommand.class);
+
     public CreateTokenCommand(TokenRequestDTO tokenRequestDTO, DatabaseService service) {
-        super(HystrixCommandGroupKey.Factory.asKey("microserviceCommands"));
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("microserviceCommands"))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withExecutionTimeoutInMilliseconds(3000)));
         this.tokenRequestDTO = tokenRequestDTO;
         this.service = service;
     }
 
     @Override
     protected String run() throws Exception {
-        System.out.println("Hystrix Command Run");
-
         service.createToken(tokenRequestDTO);
-
-        return "Token Command Submitted";
+        return "Token Command Completed";
     }
 
     @Override
     protected String getFallback() {
         System.out.println("Hystrix Command Fallback - "+getExecutionException().getMessage());
-        //getExecutionException().printStackTrace();
+        getExecutionException().printStackTrace();
+        LOGGER.error("Fallback - "+getExecutionException());
         return "Create Token Failed";
     }
 }
